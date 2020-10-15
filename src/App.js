a4810a540e21759b01b8e9774d523c8cf109745a
Lewis <lewis@webuildbots.ai>
2020-10-15 22:29:33 +0100

    basic horizontal slider in react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useRef} from 'react';
+import HorizontalScroll from 'horizontal-scroll/src/index';
+import "./App.css";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const containerRef = useRef(null);
+
+    useEffect(() => {
+        if (containerRef.current) {
+            const container = containerRef.current.querySelectorAll('.horizontal-scroll-container');
+            const blocks = containerRef.current.querySelectorAll('.block');
+
+            new HorizontalScroll({
+                container,
+                blocks,
+                isAnimated: true,
+                spring: .2,
+                skewReducer: 20,
+                skewLimit: 20,
+            });
+        }
+
+    }, [containerRef])
+
+    return (
+        <div className="App">
+            <main ref={containerRef}>
+                <div className={'horizontal-scroll-container'}>
+                    {["#74c309", "#e788fd", "#381602", "#b45a4c", "#aa7fc4"].map((backgroundColor, index) =>
+                        <div className="block" key={index}>
+                            <div className="block-inner" style={{
+                                backgroundColor,
+                            }}>
+                                <h2>Page {index + 1}</h2>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </main>
+        </div>
+    );
 }
 
 export default App;
